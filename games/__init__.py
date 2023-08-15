6c4c9b0e9bb0f0343d0e90dc7874a916119427b4
Raymond Yang <raymondyang@outlook.co.nz>
2023-08-15 21:06:36 +1200

    Add memory repository for the data layer

--- games/__init__.py
+++ games/__init__.py
@@ -1,23 +1,11 @@
 """Initialize Flask app."""
 
+from pathlib import Path
+
 from flask import Flask, render_template
 
-# TODO: Access to the games should be implemented via the repository pattern and using blueprints, so this can not
-#  stay here!
-from games.domainmodel.model import Game
-
-
-# TODO: Access to the games should be implemented via the repository pattern and using blueprints, so this can not
-#  stay here!
-def create_some_game():
-    some_game = Game(1, "Call of Duty® 4: Modern Warfare®")
-    some_game.release_date = "Nov 12, 2007"
-    some_game.price = 9.99
-    some_game.description = "The new action-thriller from the award-winning team at Infinity Ward, the creators of " \
-                            "the Call of Duty® series, delivers the most intense and cinematic action experience ever. "
-    some_game.image_url = "https://cdn.akamai.steamstatic.com/steam/apps/7940/header.jpg?t=1646762118"
-    return some_game
-
+import games.adapters.repository as repo
+from games.adapters.memory_repository import MemoryRepository, populate
 
 def create_app():
     """Construct the core application."""
@@ -25,10 +13,14 @@ def create_app():
     # Create the Flask app object.
     app = Flask(__name__)
 
+    repo.repo_instance = MemoryRepository()
+    populate(Path('games/adapters/data/games.csv'), repo.repo_instance)
+
+    # TODO: Create this route via blueprint
     @app.route('/')
     def home():
-        some_game = create_some_game()
         # Use Jinja to customize a predefined html page rendering the layout for showing a single game.
-        return render_template('gameDescription.html', game=some_game)
+        CALL_OF_DUTY_GAME_ID = 7940
+        return render_template('gameDescription.html', game=repo.repo_instance.get_game(CALL_OF_DUTY_GAME_ID))
 
     return app
---
+++ games/adapters/repository.py
@@ -0,0 +1,50 @@
+import abc
+from typing import List
+
+from games.domainmodel.model import Publisher, Genre, Game
+
+
+repo_instance = None
+
+
+class RepositoryException(Exception):
+
+    def __init__(self, message=None):
+        pass
+
+
+class AbstractRepository(abc.ABC):
+
+    @abc.abstractmethod
+    def add_publisher(self, publisher: Publisher):
+        """" Adds a Publisher to the repository. """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_publishers(self) -> List[Publisher]:
+        """ Returns a list of Publishers from the repository. """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def add_genre(self, genre: Genre):
+        """" Adds a Genre to the repository. """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_genres(self) -> List[Genre]:
+        """ Returns a list of Genres from the repository """
+        raise NotImplementedError
+  
+    @abc.abstractmethod
+    def add_game(self, game: Game):
+        """" Adds a Game to the repository. """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_game(self, id) -> Game:
+        """ Returns the Game with the given id from the repository.
+
+        If there is no Game with the given id, this method returns None.
+        """
+        raise NotImplementedError
+
