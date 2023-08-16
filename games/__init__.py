b8419775ec32b54a70a15b807e736e10bbe1a482
*jpen687* <jpen687@aucklanduni.ac.nz>
2023-08-16 22:13:24 +1200

    html files added, have not yet edited css to get the formating right but basic files are all there

--- games/__init__.py
+++ games/__init__.py
@@ -17,10 +17,14 @@ def create_app():
     populate(Path('games/adapters/data/games.csv'), repo.repo_instance)
 
     # TODO: Create this route via blueprint
-    @app.route('/')
-    def home():
+    @app.route('/gameDescription')
+    def gameDescription():
         # Use Jinja to customize a predefined html page rendering the layout for showing a single game.
         CALL_OF_DUTY_GAME_ID = 7940
         return render_template('gameDescription.html', game=repo.repo_instance.get_game(CALL_OF_DUTY_GAME_ID))
+
+    @app.route('/')
+    def home():
+        return render_template('home.html')
 
     return app
