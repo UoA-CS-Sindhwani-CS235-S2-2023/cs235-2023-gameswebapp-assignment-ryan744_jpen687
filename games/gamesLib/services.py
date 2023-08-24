f9f3ba9ebec870e96cd46d52bed2f23bfa886a54
Raymond Yang <raymondyang@outlook.co.nz>
2023-08-24 22:46:30 +1200

    Fix Genre and Publisher search categories

--- games/gamesLib/services.py
+++ games/gamesLib/services.py
@@ -39,12 +39,12 @@ def filter_games(batch_of_games, search_term, search_category):
       return batch_of_games
   def filter_fn(game):
       if search_category == 'genre':
-          genre_names = map(lambda x:x['genre_name'], game['genres'])
+          genre_names = map(lambda x:x.genre_name, game['genres'])
           if search_term.title() in genre_names:
             return True
       if search_category == 'title' and search_term.lower() in game['title'].lower():
           return True
-      if search_category == 'publisher' and search_term.lower() in game['publisher']['publisher_name'].lower():
+      if search_category == 'publisher' and search_term.lower() in game['publisher'].publisher_name.lower():
           return True
       return False
   return list(filter(filter_fn, batch_of_games))
--- tests/unit/test_gamesLib.py
+++ tests/unit/test_gamesLib.py
@@ -1,8 +1,9 @@
 from games.gamesLib.services import filter_games
+from games.domainmodel.model import Publisher, Genre, Game, Review, User, Wishlist
 
 def test_filter_games_search_term_none():
-    batch_of_games = [{'title': 'Game 1', 'genres': [{'genre_name': 'Action'}]},
-                      {'title': 'Game 2', 'genres': [{'genre_name': 'Adventure'}]}]
+    batch_of_games = [{'title': 'Game 1', 'genres': [Genre('Action')]},
+                      {'title': 'Game 2', 'genres': [Genre('Adventure')]}]
     search_term = None
     search_category = 'genre'
     
@@ -11,68 +12,68 @@ def test_filter_games_search_term_none():
     assert result == batch_of_games
 
 def test_filter_games_by_genre():
-    batch_of_games = [{'title': 'Game 1', 'genres': [{'genre_name': 'Action'}]},
-                      {'title': 'Game 2', 'genres': [{'genre_name': 'Adventure'}]}]
+    batch_of_games = [{'title': 'Game 1', 'genres': [Genre('Action')]},
+                      {'title': 'Game 2', 'genres': [Genre('Adventure')]}]
     search_term = 'Action'
     search_category = 'genre'
     
     result = filter_games(batch_of_games, search_term, search_category)
     
-    assert result == [{'title': 'Game 1', 'genres': [{'genre_name': 'Action'}]}]
+    assert result == [{'title': 'Game 1', 'genres': [Genre('Action')]}]
 
 def test_filter_games_by_genre_search_term_lower():
-    batch_of_games = [{'title': 'Game 1', 'genres': [{'genre_name': 'Action'}]},
-                      {'title': 'Game 2', 'genres': [{'genre_name': 'Adventure'}]}]
+    batch_of_games = [{'title': 'Game 1', 'genres': [Genre('Action')]},
+                      {'title': 'Game 2', 'genres': [Genre('Adventure')]}]
     search_term = 'action'
     search_category = 'genre'
     
     result = filter_games(batch_of_games, search_term, search_category)
     
-    assert result == [{'title': 'Game 1', 'genres': [{'genre_name': 'Action'}]}]
+    assert result == [{'title': 'Game 1', 'genres': [Genre('Action')]}]
 
 def test_filter_games_by_title():
-    batch_of_games = [{'title': 'Game 1', 'genres': [{'genre_name': 'Action'}]},
-                      {'title': 'Game 2', 'genres': [{'genre_name': 'Adventure'}]}]
+    batch_of_games = [{'title': 'Game 1', 'genres': [Genre('Action')]},
+                      {'title': 'Game 2', 'genres': [Genre('Adventure')]}]
     search_term = 'Game 2'
     search_category = 'title'
     
     result = filter_games(batch_of_games, search_term, search_category)
     
-    assert result == [{'title': 'Game 2', 'genres': [{'genre_name': 'Adventure'}]}]
+    assert result == [{'title': 'Game 2', 'genres': [Genre('Adventure')]}]
 
 def test_filter_games_by_title_search_term_lower():
-    batch_of_games = [{'title': 'Game 1', 'genres': [{'genre_name': 'Action'}]},
-                      {'title': 'Game 2', 'genres': [{'genre_name': 'Adventure'}]}]
+    batch_of_games = [{'title': 'Game 1', 'genres': [Genre('Action')]},
+                      {'title': 'Game 2', 'genres': [Genre('Adventure')]}]
     search_term = 'game 2'
     search_category = 'title'
     
     result = filter_games(batch_of_games, search_term, search_category)
     
-    assert result == [{'title': 'Game 2', 'genres': [{'genre_name': 'Adventure'}]}]
+    assert result == [{'title': 'Game 2', 'genres': [Genre('Adventure')]}]
 
 def test_filter_games_by_publisher():
-    batch_of_games = [{'title': 'Game 1', 'publisher': {'publisher_name': 'Publisher A'}},
-                      {'title': 'Game 2', 'publisher': {'publisher_name': 'Publisher B'}}]
+    batch_of_games = [{'title': 'Game 1', 'publisher': Publisher('Publisher A')},
+                      {'title': 'Game 2', 'publisher': Publisher('Publisher B')}]
     search_term = 'Publisher A'
     search_category = 'publisher'
     
     result = filter_games(batch_of_games, search_term, search_category)
     
-    assert result == [{'title': 'Game 1', 'publisher': {'publisher_name': 'Publisher A'}}]
+    assert result == [{'title': 'Game 1', 'publisher': Publisher('Publisher A')}]
 
 def test_filter_games_by_publisher_search_term_lower():
-    batch_of_games = [{'title': 'Game 1', 'publisher': {'publisher_name': 'Publisher A'}},
-                      {'title': 'Game 2', 'publisher': {'publisher_name': 'Publisher B'}}]
+    batch_of_games = [{'title': 'Game 1', 'publisher': Publisher('Publisher A')},
+                      {'title': 'Game 2', 'publisher': Publisher('Publisher B')}]
     search_term = 'publisher a'
     search_category = 'publisher'
     
     result = filter_games(batch_of_games, search_term, search_category)
     
-    assert result == [{'title': 'Game 1', 'publisher': {'publisher_name': 'Publisher A'}}]
+    assert result == [{'title': 'Game 1', 'publisher': Publisher('Publisher A')}]
 
 def test_filter_games_no_match():
-    batch_of_games = [{'title': 'Game 1', 'genres': [{'genre_name': 'Action'}]},
-                      {'title': 'Game 2', 'genres': [{'genre_name': 'Adventure'}]}]
+    batch_of_games = [{'title': 'Game 1', 'genres': [Genre('Action')]},
+                      {'title': 'Game 2', 'genres': [Genre('Adventure')]}]
     search_term = 'Strategy'
     search_category = 'genre'
     
