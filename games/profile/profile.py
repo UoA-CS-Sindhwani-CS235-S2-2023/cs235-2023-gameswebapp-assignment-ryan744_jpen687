76f4facc11534df97a2c0a85cc582d39060fd5e2
xyxhua99 <xhua261@aucklanduni.ac.nz>
2023-09-16 23:00:42 +1200

    Profile of user will now be able to view all the games that the user has reviewed

--- games/profile/profile.py
+++ games/profile/profile.py
@@ -13,9 +13,11 @@ profile_blueprint = Blueprint(
 @login_required
 def profile():
     favourite_games = services.get_favourite_games(session['username'], repo.repo_instance)
+    all_reviewed_games_by_user = services.get_reviews_done_by_user(session['username'], repo.repo_instance)
     return render_template(
         'profile/profile.html',
         genres=utilities.get_genres(),
         logged_in_username=logged_in_username(),
-        favourite_games=favourite_games
-      )
+        favourite_games=favourite_games,
+        reviews=all_reviewed_games_by_user,
+    )
--- games/profile/services.py
+++ games/profile/services.py
@@ -1,5 +1,12 @@
 from games.gamesLib.services import multi_games_to_dict
 
+
 def get_favourite_games(username, repo):
-  user = repo.get_user(username);
-  return multi_games_to_dict(user.favourite_games)
+    user = repo.get_user(username)
+    return multi_games_to_dict(user.favourite_games)
+
+
+def get_reviews_done_by_user(username, repo):
+    user = repo.get_user(username)
+    all_reviews_by_this_user = user.reviews #list of reviews
+    return all_reviews_by_this_user
