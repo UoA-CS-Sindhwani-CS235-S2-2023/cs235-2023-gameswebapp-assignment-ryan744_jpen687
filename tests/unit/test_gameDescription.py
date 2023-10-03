43fa767365e63002691db1d74b40bbfe5eb82835
Raymond Yang <raymondyang@outlook.co.nz>
2023-10-03 22:27:07 +1300

    Add favourites table

--- tests/unit/test_gameDescription.py
+++ tests/unit/test_gameDescription.py
@@ -3,16 +3,15 @@ from games.gameDescription.services import toggle_favourite_game_for_user, is_fa
 
 
 def test_add_favourite_game_for_user(in_memory_repo):
-    game = in_memory_repo.get_game(1)
     toggle_favourite_game_for_user(1, "jason", in_memory_repo)
-    favourite_games = in_memory_repo.get_users_favourite_games("jason")
-    assert favourite_games == [game]
+    favourite_games = in_memory_repo.get_users_favourite_game_ids("jason")
+    assert favourite_games == [1]
 
 
 def test_remove_favourite_game_for_user(in_memory_repo):
     in_memory_repo.add_users_favourite_game("jason", 1)
     toggle_favourite_game_for_user(1, "jason", in_memory_repo)
-    favourite_games = in_memory_repo.get_users_favourite_games("jason")
+    favourite_games = in_memory_repo.get_users_favourite_game_ids("jason")
     assert favourite_games == []
 
 
